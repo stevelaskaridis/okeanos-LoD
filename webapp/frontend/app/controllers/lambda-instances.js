ed60d36aa893d22aebc8cc039fcb9a6660bbd6e9
Stefanos Laskaridis <laskstef@csd.auth.gr>
2015-10-21 14:02:13 +0200

    Frontend pagination support.

--- webapp/frontend/app/controllers/lambda-instances.js
+++ webapp/frontend/app/controllers/lambda-instances.js
@@ -1,11 +1,20 @@
 import Ember from 'ember';
+import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
+  queryParams: ["page", "perPage"],
+
+  page: 1,
+  perPage: 10,
+
+  pagedContent: pagedArray('content', {pageBinding: "page", perPageBinding: "perPage"}),
+
+  totalPagesBinding: "pagedContent.totalPages",
   actions: {
     delete(lambdaInstance) {
       lambdaInstance.destroyRecord();
       // this.store.deleteRecord(lambdaInstance);
       // lambdaInstance.save();
     }
-  }
+  },
 });
